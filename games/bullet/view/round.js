b9fcb3cf66ba2a87a94beba2f8a541dad569c4c1
Michael Kearns <michael.kearns@softwire.com>
2018-07-16 20:28:36 +0100

    refactorin' every day

--- games/bullet/view/round.js
+++ games/bullet/view/round.js
@@ -3,11 +3,14 @@ const log = require('../log');
 const colours = require('./colours');
 
 module.exports.render = ({players}, pixels, {width, height}) => {
+    const renderColoursAtPixel = (rgb, x, y) => {
+        for (let c = 0; c < 3; ++c) {
+            const target = (((y * width) + x) * 3) + c;
+            pixels[target] = rgb[c];
+        }
+    }
     players.forEach(player => {
         const colour = player.alive ? colours[player.id] : [128,128,128];
-        for (let c = 0; c < 3; ++c) {
-            const target = (((player.y * width) + player.x) * 3) + c;
-            pixels[target] = colour[c];
-        }
+        renderColoursAtPixel(colour, player.x, player.y);
     });
 };
