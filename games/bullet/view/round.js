4cbc68b73ab84528921f8eddb48e1f3cbea5ef9f
Michael Kearns <michael.kearns@softwire.com>
2018-07-16 20:05:47 +0100

    world's worst snake

--- games/bullet/view/round.js
+++ games/bullet/view/round.js
@@ -7,7 +7,9 @@ module.exports.render = ({players}, pixels, {width, height}) => {
         log({x: player.x, y: player.y})
         const colour = player.alive ? colours[player.id] : [128,128,128];
         for (let c = 0; c < 3; ++c) {
-            pixels[(((player.y * width) + player.x)) + c] = colour[c];
+            const target = (((player.y * width) + player.x) * 3) + c;
+            log("writing to target " + target)
+            pixels[target] = colour[c];
         }
     });
 };
